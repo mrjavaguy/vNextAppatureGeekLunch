6a22ae9c1af3fecbda82ee409802cbc2064af622
Eric Holton <eric.holton@gmail.com>
2014-09-30 20:25:30 +0000

    Initial Commit

---
+++ vNextGeekLunch/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+
+
+namespace vNextGeekLunch
+{
+    /// <summary>
+    /// Summary description for HomeController
+    /// </summary>
+    public class HomeController : Controller
+    {
+	    public HomeController()
+	    {
+		    //
+		    // TODO: Add constructor logic here
+		    //
+	    }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Building()
+        {
+            return View();
+        }
+    }
+}
---
+++ vNextGeekLunch/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+
+namespace vNextGeekLunch
+{
+    public class Startup
+    {
+        public void Configure(IBuilder app)
+        {
+            app.UseErrorPage();
+            app.UseStaticFiles();
+            app.UseServices(services =>
+            {
+                services.AddMvc();
+            }
+            );
+
+            app.UseMvc();
+        }
+    }
+}
